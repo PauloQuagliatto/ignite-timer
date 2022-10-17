6734b2ef30a0c5a670d6989be46ec1a956a86eb0
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2022-10-17 11:31:05 -0300

    advanced in ignite classes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,98 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import { differenceInSeconds } from "date-fns";
 import { Play } from "phosphor-react";
 
-import { HomeContainer, FormContainer, TaskInput, MinutesAmountInput, CountDownContainer, Separator, StartCountDownButton } from "./styles";
+import {
+  HomeContainer,
+  FormContainer,
+  TaskInput,
+  MinutesAmountInput,
+  CountDownContainer,
+  Separator,
+  StartCountDownButton
+} from "./styles";
+
+const newCycleValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5, 'O cíclo precisa ser de, no mínimo, 5 minutos').max(60, 'O cíclo não deve exceder 60 minutos')
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleValidationSchema>;
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+}
 
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [passedSecondsAmount, setPassedSecondsAmount] = useState(0);
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  });
+
+  useEffect(() => {
+    if(activeCycle) {
+      setInterval(() => {
+        setPassedSecondsAmount(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, []);
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date()
+    };
+
+    setCycles(state => [...state, newCycle]);
+
+    reset();
+  };
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - passedSecondsAmount : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, '0');
+  const seconds = String(secondsAmount).padStart(2, '0');
+
+  const task = watch('task');
+  const isButtonDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput 
+          <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Digite o nome da sua tarefa"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
-
+            <option value='' />
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
@@ -23,6 +100,9 @@ export function Home() {
             id="minutesAmount"
             placeholder="00"
             step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount')}
           />
 
 
@@ -30,14 +110,14 @@ export function Home() {
         </FormContainer>
 
         <CountDownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountDownContainer>
 
-        <StartCountDownButton type="submit">
+        <StartCountDownButton disabled={isButtonDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountDownButton>
