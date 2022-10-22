e76c5d23e4d9a903db2c92cb708531473c425ef0
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2022-10-22 19:50:27 -0300

    logo added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { NavLink } from "react-router-dom";
 import { Timer, Scroll } from "phosphor-react";
 
+import Logo from "../../assets/Logo.svg"
+
 import { HeaderContainer } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span> logo </span>
+      <span>
+        <img
+          src={Logo}
+          alt=""
+        />
+      </span>
       <nav>
         <NavLink to="/" title="Timer">
           <Timer size={24} />
